70922a4b9a7e16d0297a3a9d1fb52d21debc862f
jellocan <christopherle.guelph@gmail.com>
2018-01-10 14:38:49 -0500

    PLAYER MOVEMENT
    HE FLIES

--- Main/Assets/PlayerController.cs
+++ Main/Assets/PlayerController.cs
@@ -1,16 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour {
+	//Movement Variables
+	public float speedForce = 50;
+	Rigidbody2D rb;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void Update ()
+	{
 		
+		if (Input.GetKey(KeyCode.D))
+		{
+			rb.velocity = new Vector2(speedForce, rb.velocity.y);
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+		else if (Input.GetKey(KeyCode.A))
+		{
+			rb.velocity = new Vector2(-speedForce, rb.velocity.y);
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+		else if (Input.GetKey(KeyCode.W))
+		{
+			rb.velocity = new Vector2(rb.velocity.x, 1);
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+		else
+			rb.velocity = new Vector2(0, rb.velocity.y);
 	}
-}
+}﻿
