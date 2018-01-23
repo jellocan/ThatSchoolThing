db305d29f81a95f737caad65af1f2e105d3b9dcd
JellocanMainPc <christopherle.guelph@gmail.com>
2018-01-23 08:21:42 -0500

    !

---
+++ Main/Assets/Health/healthdown.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthdown : MonoBehaviour {
+
+    // Use this for initialization
+    Animator animaty;
+    int health = 9;
+    void Start()
+    {
+        animaty = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+    
+            health -=(int) Time.deltaTime;
+            animaty.SetInteger("healthc", health);
+        
+
+    }
+}
